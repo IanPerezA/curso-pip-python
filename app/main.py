4849a2012b127a489e8199424d0cf6b45d56457f
IanPerezA <ipereza1702@alumno.ipn.mx>
2024-02-22 08:13:22 -0600

    Haciendo el mismo grafico de pastel pero ahora con pandas

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+  '''
+  ->sin pandas<-
   data = read_csv.read_csv('app/data.csv')
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
+  '''
+  #->Con pandas<-
+  df=pd.read_csv('app/data.csv')
+  data=df[df['Continent']== 'Africa']
+  countries=data['Country'].values
+  percentages=data['World Population Percentage'].values
+
   charts.generate_pie_chart(countries, percentages)
-  
+  '''
   country = input('Type Country => ')
   print(country)
 
@@ -20,6 +30,6 @@ def run():
     print(country)
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country'], labels, values)
-
+    '''
 if __name__ == '__main__':
   run()
